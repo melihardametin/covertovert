de9923785b4456761511b083704837026dbb2527
tunahangumus <97591351+tunahangumus@users.noreply.github.com>
2024-12-29 14:22:50 +0300

    last changes tunnniiii

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,7 +1,7 @@
 from CovertChannelBase import CovertChannelBase
 from scapy.all import IP, TCP, send, sniff
 from time import time, sleep
-import threading
+import multiprocessing
 
 class MyCovertChannel(CovertChannelBase):
     def __init__(self):
@@ -20,83 +20,70 @@ class MyCovertChannel(CovertChannelBase):
 
             print(f"Sent burst of {num_packets} packets for bit '{bit}'")
             sleep(idle_time)
-        sleep(idle_time)
-        packet = IP(dst="172.18.0.3") / TCP(dport=8000, flags="S")
-        super().send(packet)
 
 
 
     def receive(self, idle_threshold, packets_for_1, packets_for_0, log_file_name):
-        received_message = ""
-        burst_packet_count = 0
-        sender_started = False
-        last_packet_time = time()
-        sniff_socket = None
-        sender_finished = False
+        manager = multiprocessing.Manager()
+        shared_data = manager.dict()
+        shared_data['received_message'] = ""
+        shared_data['burst_packet_count'] = 0
+        shared_data['sender_started'] = False
+        shared_data['last_packet_time'] = time()
+        
 
         def packet_handler(packet):
-            nonlocal burst_packet_count, last_packet_time, received_message, sender_started
 
-            if not sender_started:
-                sender_started = True
-                last_packet_time = time()
+            if not shared_data['sender_started']:
+                shared_data['sender_started'] = True
+                shared_data['last_packet_time'] = time()
 
             if TCP in packet:
                 current_time = time()
-                idle_time = current_time - last_packet_time
+                idle_time = current_time - shared_data['last_packet_time']
 
                 if idle_time > idle_threshold:
-                    if burst_packet_count == packets_for_1:
-                        received_message += "1"
-                    elif burst_packet_count == packets_for_0:
-                        received_message += "0"
+                    if shared_data['burst_packet_count'] == packets_for_1:
+                        shared_data['received_message'] += "1"
+                    elif shared_data['burst_packet_count'] == packets_for_0:
+                        shared_data['received_message'] += "0"
 
-                    print(f"Decoded burst: {burst_packet_count} packets -> '{received_message[-1:]}'")
-                    burst_packet_count = 0
+                    print(f"Decoded burst: {shared_data['burst_packet_count']} packets -> '{shared_data['received_message'][-1:]}'")
+                    shared_data['burst_packet_count'] = 0
 
-                burst_packet_count += 1
-                last_packet_time = current_time
+                shared_data['burst_packet_count'] += 1
+                shared_data['last_packet_time'] = current_time
         
-
-        def monitor_terminated():
-            nonlocal last_packet_time, sender_started, burst_packet_count, received_message, sniff_thread, sender_finished
-            while True:
-                sleep(0.6)
-                if sender_started and (time() - last_packet_time) > 0.6:
-                    if burst_packet_count == packets_for_1:
-                        received_message += "1"
-                    elif burst_packet_count == packets_for_0:
-                        received_message += "0"
-
-                    print(f"Decoded burst: {burst_packet_count} packets -> '{received_message[-1:]}'")
-                    print("No packets received in the last 0.6 seconds. Terminating sniffing.")
-                    sender_finished = True
-                    break
-                    
-
         # Start sniffing with socket access
         def start_sniffing():
-            nonlocal sniff_socket
             print("sniff started")
             sniff_socket = sniff(
                 filter=f"tcp and src host 172.18.0.2 and dst port 8000",
-                prn=packet_handler,
-                stop_filter=lambda _: sender_finished == True
+                prn=packet_handler
             )
             print("sniff ended")
 
-        sniff_thread = threading.Thread(target=start_sniffing)
+        sniff_thread = multiprocessing.Process(target=start_sniffing, args=())
         sniff_thread.start()
 
-        monitor_thread = threading.Thread(target=monitor_terminated)
-        monitor_thread.start()
+        while True:
+                sleep(0.6)
+                if shared_data['sender_started'] and (time() - shared_data['last_packet_time']) > 0.6:
+                    if shared_data['burst_packet_count'] == packets_for_1:
+                        shared_data['received_message'] += "1"
+                    elif shared_data['burst_packet_count'] == packets_for_0:
+                        shared_data['received_message'] += "0"
 
-        monitor_thread.join()
+                    print(f"Decoded burst: {shared_data['burst_packet_count']} packets -> '{shared_data['received_message'][-1:]}'")
+                    print("No packets received in the last 0.6 seconds. Terminating sniffing.")
+                    sniff_thread.terminate()
+                    break
+
         sniff_thread.join()
 
         decoded_message = "".join(
-            self.convert_eight_bits_to_character(received_message[i:i + 8])
-            for i in range(0, len(received_message), 8)
+            self.convert_eight_bits_to_character(shared_data['received_message'][i:i + 8])
+            for i in range(0, len(shared_data['received_message']), 8)
         )
 
         self.log_message(decoded_message, log_file_name)
